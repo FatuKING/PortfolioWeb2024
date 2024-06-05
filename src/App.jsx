3e3404b88b350e16bcb6761e55f1ac98c78dd75e
Facundo Cozzani <cozzanifacundo@gmail.com>
2024-06-04 23:11:48 -0300

    Icons para las habilidades

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Navbar } from './components/Navbar.jsx'
 import { AboutMe } from './components/AboutMe.jsx'
 import { Proyect } from './components/Proyect.jsx'
 import { useState } from 'react'
+import { Skill } from './components/Skill.jsx'
+import { IconJavascript } from './components/icons.jsx'
 
 function App () {
   const [proyect, setProyect] = useState(true)
@@ -42,6 +44,12 @@ function App () {
 
         <section className='w-4/5 xl:w-3/4 2xl:w-1/2 pt-32'>
           <h2 className='text-lg lg:text-xl xl:text-2xl  pb-3 font-bold' id='Habilidades'> <i class='bi bi-code-slash font-bold' /> Habilidades</h2>
+
+          <article>
+            <Skill icon='IconJavascript' skill='JavaScript' />
+            <Skill icon='IconReact' skill='React' />
+            <Skill />
+          </article>
         </section>
       </main>
 
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,6 +1,23 @@
-export function Skill ({ skill }) {
+import { IconJavascript, IconReact, IconTailwind, IconHtml, IconCss, IconBootstrap, IconCShar, IconJava, IconNodejs, IconMicrosoftSqlServer, IconMySql, IconPostgreSql } from './icons.jsx'
+
+export function Skill ({ skill, icon }) {
   return (
     <>
+      <div>
+        <IconJavascript />
+        <IconTailwind />
+        <IconReact />
+        <IconCss />
+        <IconHtml />
+        <IconBootstrap />
+        <IconCShar />
+        <IconJava />
+        <IconNodejs />
+        <IconMicrosoftSqlServer />
+        <IconPostgreSql />
+        <IconMySql />
+        <p>{skill}</p>
+      </div>
     </>
   )
 }
