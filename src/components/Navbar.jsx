25455759173df3274a0d55d6770be6974a14f652
fcozzani <fcozzani@SISTEMAS013>
2025-04-29 11:57:31 -0300

    Refactor: Modifico el navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,20 +8,22 @@ export function Navbar () {
   return (
     <>
       <header className='flex items-center justify-center fixed w-full bg-black/20 backdrop-blur-lg z-10 shadow-sm shadow-blue-950'>
-        <nav className='w-4/5 xl:w-3/4 2xl:w-1/2 p-1 z-20 h-14 flex justify-center sm:justify-between items-center'>
+        <div className='w-4/5 xl:w-3/4 2xl:w-1/2 p-1 z-20 h-14 flex justify-between items-center'>
           <strong className='hidden sm:inline-block text-xl font-semibold title'>Facundo Cozzani</strong>
+          
+          <nav className='flex items-center gap-4'>
+            <ul className='flex gap-3 bg-transparent'>
+              {menuItems.map((item, index) => (
+                <li key={index} className='font-medium text-base hover:text-purple-700'><a href={item.url}>{item.name}</a></li>
+              ))}
+            </ul>
 
-          <ul className='flex gap-3 bg-transparent'>
-            {menuItems.map((item, index) => (
-              <li key={index} className='font-medium text-base hover:text-purple-700'><a href={item.url}>{item.name}</a></li>
-            ))}
-          </ul>
-
-          <div className="flex gap-3">
-            <i class="bi bi-github"></i>
-            <i class="bi bi-linkedin"></i>
-          </div>
-        </nav>
+            <div className="flex gap-2 text-lg">
+              <a href="https://github.com/fatuking" title='Github' target="_blank"><i className="bi bi-github"></i></a>
+              <a href="https://www.linkedin.com/in/facundocozzani/" title="Linkedin" target="_blank"><i className="bi bi-linkedin"></i></a>
+            </div>
+          </nav>
+        </div>
       </header>
     </>
   )
